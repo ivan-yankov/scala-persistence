ff3466394e00de38dbeb16761f5520d48109e954
Ivan Yankov <ivan.iankov@gmail.com>
2020-07-24 23:31:58 +0300

    Improve tree data structure

--- src/main/scala/org/yankov/datastructures/Tree.scala
+++ src/main/scala/org/yankov/datastructures/Tree.scala
@@ -2,12 +2,12 @@ package org.yankov.datastructures
 
 import scala.annotation.tailrec
 
-case class Node[T](data: Option[T], children: List[Node[T]]) {
+case class Node[T](data: T) {
   def asFlatten(level: Int, index: Int, parentIndex: Int): FlattenNode[T] = FlattenNode(
     level = level,
     index = index,
     parentIndex = parentIndex,
-    Node(data, children)
+    Node(data)
   )
 }
 
@@ -20,7 +20,9 @@ case class FlattenNode[T](level: Int, index: Int, parentIndex: Int, node: Node[T
   )
 }
 
-case class Tree[T](root: Node[T]) {
+case class Tree[T](root: Node[T],
+                   getChildren: Node[T] => List[Node[T]],
+                   aggregate: (Node[T], List[Node[T]]) => Node[T]) {
   @tailrec
   final def flat(node: FlattenNode[T] = root.asFlatten(0, -1, -1),
                  index: Int = 0,
@@ -28,7 +30,7 @@ case class Tree[T](root: Node[T]) {
                  resultAcc: List[FlattenNode[T]] = List()): List[FlattenNode[T]] = {
     val indexedNode = node.withIndex(index)
     val newResultAcc = indexedNode :: resultAcc
-    (indexedNode.node.children, acc) match {
+    (getChildren(indexedNode.node), acc) match {
       case (Nil, Nil) => newResultAcc.reverse
       case (Nil, x :: rest) => flat(x, index + 1, rest, newResultAcc)
       case (children, xs) =>
@@ -52,24 +54,24 @@ case class Tree[T](root: Node[T]) {
     if (result.isDefined) Option(result.get.node) else Option.empty
   }
 
-  def merge(parentMatch: Node[T] => Boolean, that: Tree[T])
-           (implicit sortChildren: (Node[T], Node[T]) => Boolean): Tree[T] = {
+  def merge(parentMatch: T => Boolean, that: Tree[T]): Tree[T] = {
     val thisFlatten = flat()
-    val parent = thisFlatten.find(x => parentMatch(x.node))
+    val parent = thisFlatten.find(x => parentMatch(x.node.data))
     if (parent.isEmpty) this
     else {
       val thatRootLevel = parent.get.level + 1
       val thatRootIndex = thisFlatten.size
       val thatRootParentIndex = parent.get.index
       val thatFlatten = that.flat(that.root.asFlatten(thatRootLevel, thatRootIndex, thatRootParentIndex))
-      Tree.build(thisFlatten.appendedAll(thatFlatten))
+      Tree.build(thisFlatten.appendedAll(thatFlatten), getChildren, aggregate)
     }
   }
 }
 
 object Tree {
-  def build[T](flatten: List[FlattenNode[T]])
-              (implicit sortChildren: (Node[T], Node[T]) => Boolean): Tree[T] = {
+  def build[T](flatten: List[FlattenNode[T]],
+               getChildren: Node[T] => List[Node[T]],
+               aggregate: (Node[T], List[Node[T]]) => Node[T]): Tree[T] = {
     @tailrec
     def iterate(nodes: List[FlattenNode[T]], acc: List[FlattenNode[T]]): List[FlattenNode[T]] = {
       if (nodes.isEmpty) acc
@@ -86,7 +88,8 @@ object Tree {
             x._1.level,
             x._1.index,
             x._1.parentIndex,
-            Node(x._1.node.data, x._2.map(y => y.node).sortWith(sortChildren))))
+            aggregate(x._1.node, x._2.map(y => y.node))
+          ))
           .toList
           .appendedAll(leaves)
           .sortWith((x, y) => x.index <= y.index)
@@ -98,6 +101,6 @@ object Tree {
     val depth = flatten.map(x => x.level).max
 
     val result = iterate(flatten.filter(x => x.level != depth), flatten.filter(x => x.level == depth))
-    Tree(result.head.node)
+    Tree(result.head.node, getChildren, aggregate)
   }
 }
--- src/test/scala/org/yankov/datastructures/TreeTest.scala
+++ src/test/scala/org/yankov/datastructures/TreeTest.scala
@@ -3,112 +3,108 @@ package org.yankov.datastructures
 import org.scalatest.{Matchers, WordSpec}
 
 class TreeTest extends WordSpec with Matchers {
-  implicit def sortChildren: (Node[String], Node[String]) => Boolean =
-    (x, y) => x.data.get.compareTo(y.data.get) <= 0
+  case class StringNode(name: String, children: List[StringNode] = List())
 
-  private def createTree(): Tree[String] = {
-    val c = List.tabulate(8)(x => Node(Option(s"C$x"), List()))
-    val bChildren = List(
-      List(c.head, c(1), c(2)),
-      List(c(3), c(4), c(5)),
-      List(c(6), c(7))
-    )
-    val b = List.tabulate(3)(x => Node(Option(s"B$x"), bChildren(x)))
-    val aChildren = List(
-      List(b.head, b(1)),
-      List(b(2)),
-      List()
-    )
-    val a = List.tabulate(3)(x => Node(Option(s"A$x"), aChildren(x)))
-    Tree(Node(Option(s"ROOT"), a))
-  }
+  private def getChildren(parent: Node[StringNode]): List[Node[StringNode]] =
+    parent.data.children.map(y => Node(y))
 
-  private def createMergedTree1: Tree[String] = {
-    val c = List(
-      Node(Option("C0"), List()),
-      Node(Option("C1"), List()),
-      Node(Option("C2"), List()),
-      Node(Option("C3"), List()),
-      Node(Option("C4"), List()),
-      Node(Option("C5"), List()),
-      Node(Option("C6"), List(Node(Option("C6C"), List(
-        Node(Option("C6C0"), List()),
-        Node(Option("C6C1"), List()),
-        Node(Option("C6C2"), List())
-      )))),
-      Node(Option("C7"), List()),
-    )
-    val bChildren = List(
-      List(c.head, c(1), c(2)),
-      List(Node(Option("B1C"), List()), c(3), c(4), c(5)),
-      List(c(6), c(7))
-    )
-    val b = List.tabulate(3)(x => Node(Option(s"B$x"), bChildren(x)))
-    val aChildren = List(
-      List(b.head, b(1)),
-      List(b(2)),
-      List(Node(Option("A2C"), List()))
-    )
-    val a = List.tabulate(3)(x => Node(Option(s"A$x"), aChildren(x)))
-    Tree(Node(Option("ROOT"), a.appended(Node(Option("RC"), List()))))
-  }
+  private def aggregate(parent: Node[StringNode], children: List[Node[StringNode]]): Node[StringNode] =
+    Node(StringNode(parent.data.name, children.map(y => y.data).sortWith((x, y) => x.name.compareTo(y.name) <= 0)))
 
-  private def createMergedTree2: Tree[String] = {
-    val subTree = createTree()
-
-    val c = List.tabulate(8)(x => Node(Option(s"C$x"), List()))
-    val bChildren = List(
-      List(c.head, c(1), c(2), subTree.root),
-      List(c(3), c(4), c(5)),
-      List(c(6), c(7))
-    )
-    val b = List.tabulate(3)(x => Node(Option(s"B$x"), bChildren(x)))
-    val aChildren = List(
-      List(b.head, b(1)),
-      List(b(2)),
-      List()
-    )
-    val a = List.tabulate(3)(x => Node(Option(s"A$x"), aChildren(x)))
-    Tree(Node(Option("ROOT"), a))
-  }
-
-  private def createFlattenTree: List[FlattenNode[String]] = {
+  private def createStringNode(): StringNode = StringNode(
+    "ROOT",
     List(
-      FlattenNode(0, 0, -1, Node(Option("ROOT"), List(Node(Option("A0"), List(Node(Option("B0"), List(Node(Option("C0"), List()), Node(Option("C1"), List()), Node(Option("C2"), List()))), Node(Option("B1"), List(Node(Option("C3"), List()), Node(Option("C4"), List()), Node(Option("C5"), List()))))), Node(Option("A1"), List(Node(Option("B2"), List(Node(Option("C6"), List()), Node(Option("C7"), List()))))), Node(Option("A2"), List())))),
-      FlattenNode(1, 1, 0, Node(Option("A0"), List(Node(Option("B0"), List(Node(Option("C0"), List()), Node(Option("C1"), List()), Node(Option("C2"), List()))), Node(Option("B1"), List(Node(Option("C3"), List()), Node(Option("C4"), List()), Node(Option("C5"), List())))))),
-      FlattenNode(2, 2, 1, Node(Option("B0"), List(Node(Option("C0"), List()), Node(Option("C1"), List()), Node(Option("C2"), List())))),
-      FlattenNode(3, 3, 2, Node(Option("C0"), List())),
-      FlattenNode(3, 4, 2, Node(Option("C1"), List())),
-      FlattenNode(3, 5, 2, Node(Option("C2"), List())),
-      FlattenNode(2, 6, 1, Node(Option("B1"), List(Node(Option("C3"), List()), Node(Option("C4"), List()), Node(Option("C5"), List())))),
-      FlattenNode(3, 7, 6, Node(Option("C3"), List())),
-      FlattenNode(3, 8, 6, Node(Option("C4"), List())),
-      FlattenNode(3, 9, 6, Node(Option("C5"), List())),
-      FlattenNode(1, 10, 0, Node(Option("A1"), List(Node(Option("B2"), List(Node(Option("C6"), List()), Node(Option("C7"), List())))))),
-      FlattenNode(2, 11, 10, Node(Option("B2"), List(Node(Option("C6"), List()), Node(Option("C7"), List())))),
-      FlattenNode(3, 12, 11, Node(Option("C6"), List())),
-      FlattenNode(3, 13, 11, Node(Option("C7"), List())),
-      FlattenNode(1, 14, 0, Node(Option("A2"), List()))
+      StringNode(
+        "A0",
+        List(
+          StringNode("B0", List(StringNode("C0"), StringNode("C1"), StringNode("C2"))),
+          StringNode("B1", List(StringNode("C3"), StringNode("C4"), StringNode("C5")))
+        )
+      ),
+      StringNode(
+        "A1",
+        List(
+          StringNode("B2", List(StringNode("C6"), StringNode("C7")))
+        )
+      ),
+      StringNode("A2")
     )
+  )
+
+  private def createTree(root: StringNode = createStringNode()): Tree[StringNode] = {
+    Tree(
+      Node(root),
+      getChildren,
+      aggregate
+    )
+  }
+
+  private def createMergedTree1: Tree[StringNode] = {
+    val root = StringNode(
+      "ROOT",
+      List(
+        StringNode(
+          "A0",
+          List(
+            StringNode("B0", List(StringNode("C0"), StringNode("C1"), StringNode("C2"))),
+            StringNode("B1", List(StringNode("B1C"), StringNode("C3"), StringNode("C4"), StringNode("C5")))
+          )
+        ),
+        StringNode(
+          "A1",
+          List(
+            StringNode("B2", List(StringNode("C6", List(StringNode("C6C", List(StringNode("C6C0"), StringNode("C6C1"), StringNode("C6C2"))))), StringNode("C7")))
+          )
+        ),
+        StringNode("A2", List(StringNode("A2C"))),
+        StringNode("RC")
+      )
+    )
+
+    createTree(root)
+  }
+
+  private def createMergedTree2: Tree[StringNode] = {
+    val root = StringNode(
+      "ROOT",
+      List(
+        StringNode(
+          "A0",
+          List(
+            StringNode("B0", List(StringNode("C0"), StringNode("C1"), StringNode("C2"), createStringNode())),
+            StringNode("B1", List(StringNode("C3"), StringNode("C4"), StringNode("C5")))
+          )
+        ),
+        StringNode(
+          "A1",
+          List(
+            StringNode("B2", List(StringNode("C6"), StringNode("C7")))
+          )
+        ),
+        StringNode("A2")
+      )
+    )
+
+    createTree(root)
   }
 
   "printToString should succeed" in {
-    createTree().printToString(x => x.data.get) shouldBe "ROOT\n  A0\n    B0\n      C0\n      C1\n      C2\n    B1\n      C3\n      C4\n      C5\n  A1\n    B2\n      C6\n      C7\n  A2"
+    createTree().printToString(x => x.data.name) shouldBe "ROOT\n  A0\n    B0\n      C0\n      C1\n      C2\n    B1\n      C3\n      C4\n      C5\n  A1\n    B2\n      C6\n      C7\n  A2"
   }
 
   "find should succeed for all nodes" in {
     val tree = createTree()
     List("ROOT", "A0", "A1", "A2", "B0", "B1", "B2", "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7")
-      .map(x => (x, tree.find(y => y.data.get.equals(x))))
-      .forall(z => z._2.isDefined && z._1.equals(z._2.get.data.get)) shouldBe true
+      .map(x => (x, tree.find(y => y.data.name.equals(x))))
+      .forall(z => z._2.isDefined && z._1.equals(z._2.get.data.name)) shouldBe true
   }
 
   "find should fail to find node, which does not exist" in {
-    createTree().find(x => x.data.get.equals("SOMETHING")).isEmpty shouldBe true
+    createTree().find(x => x.data.name.equals("SOMETHING")).isEmpty shouldBe true
   }
 
   "flat should succeed and return list of nodes with corresponding flatten key in the tree" in {
-    val result = createTree().flat().map(x => (List(x.level, x.index, x.parentIndex), x.node.data.get))
+    val result = createTree().flat().map(x => (List(x.level, x.index, x.parentIndex), x.node.data.name))
 
     result.size shouldBe 15
 
@@ -133,40 +129,36 @@ class TreeTest extends WordSpec with Matchers {
   }
 
   "flat subtree should succeed" in {
-    val flatten = createTree().merge(x => x.data.get.equals("B0"), createTree()).flat()
-    Tree.build(flatten) shouldBe createMergedTree2
+    val flatten = createTree().merge(x => x.name.equals("B0"), createTree()).flat()
+    Tree.build(flatten, getChildren, aggregate).root shouldBe createMergedTree2.root
   }
 
   "build should succeed" in {
-    Tree.build(createFlattenTree) shouldBe createTree()
+    Tree.build(createTree().flat(), getChildren, aggregate).root shouldBe createTree().root
   }
 
   "merge should succeed in chain merges" in {
     val trees = List(
-      Tree(Node(Option("RC"), List())),
-      Tree(Node(Option("A2C"), List())),
-      Tree(Node(Option("B1C"), List())),
-      Tree(Node(Option("C6C"), List(
-        Node(Option("C6C0"), List()),
-        Node(Option("C6C1"), List()),
-        Node(Option("C6C2"), List())))
-      )
+      Tree(Node(StringNode("RC")), getChildren, aggregate),
+      Tree(Node(StringNode("A2C")), getChildren, aggregate),
+      Tree(Node(StringNode("B1C")), getChildren, aggregate),
+      Tree(Node(StringNode("C6C", List(StringNode("C6C0"), StringNode("C6C1"), StringNode("C6C2")))), getChildren, aggregate)
     )
 
     val tree = createTree()
-      .merge(x => x.data.get.equals("ROOT"), trees.head)
-      .merge(x => x.data.get.equals("A2"), trees(1))
-      .merge(x => x.data.get.equals("B1"), trees(2))
-      .merge(x => x.data.get.equals("C6"), trees(3))
+      .merge(x => x.name.equals("ROOT"), trees.head)
+      .merge(x => x.name.equals("A2"), trees(1))
+      .merge(x => x.name.equals("B1"), trees(2))
+      .merge(x => x.name.equals("C6"), trees(3))
 
-    tree shouldBe createMergedTree1
+    tree.root shouldBe createMergedTree1.root
   }
 
   "merge should succeed in merging subtree" in {
-    createTree().merge(x => x.data.get.equals("B0"), createTree()) shouldBe createMergedTree2
+    createTree().merge(x => x.name.equals("B0"), createTree()).root shouldBe createMergedTree2.root
   }
 
   "merge should return same tree if parent is not found" in {
-    createTree().merge(x => x.data.get.equals("SOMETHING"), Tree(Node(Option("CHILD"), List()))) shouldBe createTree()
+    createTree().merge(x => x.name.equals("SOMETHING"), Tree(Node(StringNode("CHILD")), getChildren, aggregate)).root shouldBe createTree().root
   }
 }
